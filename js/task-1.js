b3ca94e6c12a775edcdf5d6a4ff08caa27a3dda9
MaksimChepurko <maksimchepurko490@gmail.com>
2025-01-05 16:44:40 +0100

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,1 @@
-return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-
-function makeTransaction(quantity, pricePerDroid) {
-    return totalPrice = quantity * pricePerDroid;
-}
-
-
 
