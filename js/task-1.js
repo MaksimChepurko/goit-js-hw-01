f64fbe530489cd82eb2efc298e616e99baba6661
MaksimChepurko <maksimchepurko490@gmail.com>
2025-01-06 02:31:03 +0100

    done 2

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,8 @@
 function makeTransaction(quantity, pricePerDroid) {
-  return `You ordered ${quantity} droids worth ${
-    quantity * pricePerDroid
-  } credits!`;
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
-console.log(makeTransaction(5, 3000)); 
-console.log(makeTransaction(3, 1000)); 
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
 console.log(makeTransaction(10, 500));
